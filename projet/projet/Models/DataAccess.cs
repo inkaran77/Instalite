f7a68a6317b70cca630d9e2ecab9e6fd5f09cede
Inkaran Thuraiyappah <inkaran.thuraiyappah@gmail.com>
2018-04-06 18:31:52 +0200

    initialisation Web Api

---
+++ projet/projet/Models/DataAccess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+//using MongoDB.Driver.Builders;
+using System.Collections.Generic;
+
+namespace projet.Models
+{
+    public class DataAccess
+    {
+        MongoClient _client;
+        // MongoServer _server;
+        IMongoDatabase _db;
+
+        public DataAccess()
+        {
+            _client = new MongoClient("mongodb://localhost:27017");
+            _db=_client.GetDatabase("Instalite");
+
+            //_server = _client.GetServer();
+           // _db = _server.GetDatabase("EmployeeDB");
+        }
+
+        public List<User> GetAllUsers()
+        {
+            List<User> allUsers = new List<User>();
+            var result=_db.GetCollection<User>("user").Find(new BsonDocument());
+
+            foreach (var document in result.ToEnumerable())
+            {
+                allUsers.Add(document);
+            }
+
+            return allUsers;
+        }
+
+
+        public void AddUser(User u)
+        {
+            _db.GetCollection<User>("user").InsertOne(u);   
+        }
+ 
+
+    }
+}
---
+++ projet/projet/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace projet.Models
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonElement("UserId")]
+        public string UserId { get; set; }
+
+        [BsonElement("Password")]
+        public string Password { get; set; }
+}
+}
