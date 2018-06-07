2d4a2e911ce83b7bba2e3749b052fb1289017e90
SkySurf <elanbari.mohamed@gmail.com>
2018-06-07 12:13:37 +0200

    Dicovery Page

---
+++ front/instaLite/src/pages/Discovery.vue
@@ -0,0 +1,175 @@
+
+<template>
+  <div class="content">
+    <div class="md-layout">
+      <div class="md-layout-item">
+        <md-card>
+          <md-card-header data-background-color="blue">
+            <h4 class="title">Discovery</h4>
+            <p class="category"></p>
+          </md-card-header>
+          <md-card-content>
+
+             <!-- Add icon library -->
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+            <div id="discovery">
+
+
+              <!-- list of all users -->
+              <li class="users-id" v-for="(user, index) in userslist">
+                <div class="Picture">
+                  <avatar :image='user.UrlPhoto' size ="100"></avatar>
+                </div>
+
+                 <div class="profile_name" >
+                   <h3>{{user.First_Name}} {{user.Last_Name}}</h3>
+                 </div>
+
+                <div class="drop_container">
+                  <div class="dropdown1">
+                    <button class="buttonload" v-on:click="follow(user.UserId)">
+                        Follow
+
+                    </button>
+                  </div>
+                </div>
+              </li>
+
+
+            </div>
+
+           </md-card-content>
+         </md-card>
+       </div>
+     </div>
+   </div>
+ </template>
+<script>
+
+import Avatar from 'vue-avatar-component'
+
+export default{
+
+  components: { Avatar },
+
+  props: {
+    dataBackgroundColor: {
+      type: String,
+      default: ''
+    },
+  },
+  name: 'app',
+  data () {
+    return {
+      userslist:null,
+
+    }
+  },
+
+  methods:{
+
+
+    follow : function(Id){
+
+       this.$http.delete('http://localhost:5000/Instalite/Follow',{
+        Id : this.UserId
+      },{
+        headers: {
+          'Authorization': 'Bearer '+ localStorage.token
+        }}).then(response =>{
+        this.userslist.slice(index, 1)
+        console.log(response.status)
+        })
+    }
+
+      },
+
+mounted:function(){
+
+    this.$http.get('http://localhost:5000/Instalite/GetAllUsers',{ headers: {
+          'Authorization': 'Bearer '+ localStorage.token
+        }
+        }).then(response=> {
+
+       this.userslist = response.data.ListUsers
+        console.log(response.data)
+  });
+
+
+
+      },
+}
+</script>
+<style>
+  .buttonload {
+    border-radius: 5px;
+    color : black;
+    }
+  .fa-spin {
+    color : black;
+    }
+
+
+  .users-id{
+    background:#FFFFFF;
+    border:2px solid #CCCCCC;
+    font-weight:bold;
+    color:#FFFFFF;
+    margin:auto;
+
+  }
+  .Picture{
+    display: inline-block;
+    vertical-align: middle;
+    color:black;
+    margin-top: 1%;
+    margin-bottom: 1%;
+    margin-left:1%
+  }
+
+  .profile_name{
+    display: inline-block;
+    margin-left: 1%;
+    margin-bottom: 1%;
+    color:black;
+    width: 76%;
+    margin:0;
+  }
+
+ .drop_container{
+    display: inline-block;
+    position:relative;
+
+  }
+
+  .dropdown1 button{
+    border:none;
+    padding:8px 20px;
+    background-color:#DDDDDD;
+    color:#333333;
+    transition:.3s;
+    cursor:pointer;
+    display:inline-block;
+    position:relative;
+
+
+  }
+
+  .dropdown1:hover button{
+    background-color:#BBBBBB;
+  }
+
+  .dropdown1 div{
+    background-color:#fff;
+    box-shadow:0 4px 8px rgba(0,0,0,0.2);
+    z-index:1;
+    visibility:hidden;
+    position:absolute;
+    min-width:100%;
+    opacity:0;
+    transition:.3s;
+  }
+
+
+</style>
