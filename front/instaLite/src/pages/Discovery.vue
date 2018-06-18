55bd2928a31152ad2383396faef1721c77af1dd2
SkySurf <elanbari.mohamed@gmail.com>
2018-06-18 15:31:16 +0200

    Design ++ and discovery search bar

--- front/instaLite/src/pages/Discovery.vue
+++ front/instaLite/src/pages/Discovery.vue
@@ -14,18 +14,27 @@
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
             <div id="discovery">
+              <!-- search module -->
 
+              <div class="searchfield" >
+              <input type="text" v-model="search" placeholder=" Look for Users " id="s"/>
+              </div>
 
               <!-- list of all users -->
-              <li class="users-id" v-for="user in userslist">
+              <div class="users-id" v-for="user in filterusers">
+                <div class="UserDisplay">
                 <div class="Picture">
                   <avatar :image='user.UrlPhoto' :size ="100"></avatar>
                 </div>
 
                  <div class="profile_name" >
-                   <a v-on:click="getUserInfo(user.UrlPhoto)" href="#" style="color:#000000"><h3>{{user | fullNAme}}</h3></a>
+                   <a v-on:click="getUserInfo(user.UrlPhoto), toggle(user.UserId)" href="#" style="color:#000000"><h3>{{user | fullNAme}}</h3></a>
+                  <div>
+                 <transition name="fade">
+                  <p class="UserInfo" v-if="UserId2==user.UserId&&show" >Posts: {{posts}} &emsp; Followers: {{followers}} &emsp; Followings : {{followings}} </p>
+                 </transition>
+                  </div>
                  </div>
-
                 <div class="drop_container">
                   <div class="dropdown1">
                     <button class="buttonload" v-on:click="follow(user.UserId)">
@@ -33,7 +42,9 @@
                     </button>
                   </div>
                 </div>
-              </li>
+                </div>
+
+              </div>
 
 
             </div>
@@ -61,9 +72,15 @@ export default{
   name: 'app',
   data () {
     return {
-      userslist:null,
+      userslist:[],
       urlphoto :'',
-      UserId:''
+      UserId:'',
+      followers:null,
+      followings :null ,
+      posts : null,
+      show:false,
+      UserId2 : null,
+      search:''
 
     }
   },
@@ -98,6 +115,9 @@ export default{
         ,
         headers: {'Authorization': 'Bearer '+ this.$cookies.get("token")}
         }).then(response =>{
+          this.followers = response.data.Followers.ListUsers.length
+          this.followings = response.data.Followings.ListUsers.length
+          this.posts = response.data.List_post.length
         console.log(response.data)
       })
     },
@@ -110,9 +130,27 @@ export default{
        this.userslist = response.data.ListUsers
         console.log(response.data)
   });
+    },
+
+    toggle : function(Id){
+
+      this.UserId2= Id;
+      this.show = !this.show;
     }
 
       },
+
+    computed : {
+
+      filterusers : function(){
+
+        return this.userslist.filter((user)=> {
+
+        return user.UserId.match(this.search);
+        });
+      }
+
+    },
 
 mounted:function(){
 
@@ -133,10 +171,12 @@ mounted:function(){
 
   .users-id{
     background:#FFFFFF;
-    border:2px solid #CCCCCC;
+    border-bottom:1px solid #BBBBBB;
     font-weight:bold;
     color:#FFFFFF;
     margin:auto;
+    text-align: center;
+    width:89%;
 
   }
   .Picture{
@@ -150,11 +190,13 @@ mounted:function(){
 
   .profile_name{
     display: inline-block;
+    position: relative;
     margin-left: 1%;
     margin-bottom: 1%;
     color:black;
     width: 76%;
     margin:0;
+    text-align: left;
   }
 
  .drop_container{
@@ -168,7 +210,7 @@ mounted:function(){
     padding:8px 20px;
     background-color:#DDDDDD;
     color:#333333;
-    transition:.3s;
+    transition:.5s;
     cursor:pointer;
     display:inline-block;
     position:relative;
@@ -191,5 +233,40 @@ mounted:function(){
     transition:.3s;
   }
 
+  .fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+  .UserInfo{
+    color:#333333;
+    font-style:italic;
+    position: absolute;
+    margin-left: 0.5%
+  }
+
+
+/* Search styles */
+
+#s { background: rgba(3, 175, 255, 0.7)
+url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAAUCAYAAABvVQZ0AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAQBJREFUeNqslI0RgyAMhdENWIEVWMEVXIGO0BW6Ah2hHcGOoCPYEewINFzBe9IA9id37w4kfEZesHHOCSYUqSPJML+RJlELDwN1pMHxMZNMkr8RTgyz2YPH5LmtwXpIHkOFmKhIlxowDmYAycKnHAHYcTCsSpXOJCie6YWDnXKLGeHLN2stGaqDsXXrX3GFcYcLrfhjtKEhffQ792gYT2nT6pJDjCw4z7ZGdGipOIqNbXIwFUARmCbKpMfYxsWJBmCEDoW7+gYUTAU2s3HJrK3AJvMLkqGHFLgWXTckm+SfSQexs+tLRqwVfgvjgMsvMAT689S5M/sk/I14kO5PAQYAuk6L1q+EdHMAAAAASUVORK5CYII=)
+no-repeat 14px 14px;
+text-indent: 1em; display: inline-block; border: 0 none; width: 0; height: 3em;
+border-radius: 3em; -webkit-transition: .5s; transition: .5s; outline: none; padding: 1em 1.5em; cursor: pointer;
+-webkit-appearance: none; font-weight: inherit; font-size: inherit; font-family: inherit; color: #000;
+vertical-align: baseline;}
+
+input[type=search]::-webkit-search-cancel-button { -webkit-appearance: none;}
+
+#s:hover, #s:focus { background: #fff url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAAUCAYAAABvVQZ0AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAT5JREFUeNqsVLtOw0AQtIMlRJHCEhUVMg398QEUSZnSfILzCXxDPsFu6XAJHWnTcS1lWsprKdmLxtKwvjVBYaTV7cm+udnX5fPb+yyBSmwhVmK/FfPZLyjUPhI8YtXYi23EOovs7PzyevAbsWeoGg5HNUHsCipX8F9TZDOstVgLPxIsxW6w3sHv6dJ2StkLbh6IPtR/AWRfSIET20H9D2U1hfaAgxY2KMagcBSmg9/rmwx0lBqTzGfHoVfVHxXgXzCjHNRHnnHke4vMGc2q0RBR0GSeCLlpLaJGFWKUszVuib32nih7iTFrjXAPyGnQ48c3Gu5AOVlMtMk6NZuf+FiC+AIhV0T+pBQ5ntXceIJKqKko2duJ2TwoLAz5QTVnagJaXWEO8y/wSMuKH9RTJoCTHyNZFidOUEfNu/8WYAAOXUT04MOtlwAAAABJRU5ErkJggg==)
+14px 14px no-repeat;}
+
+#s:focus { width: 25%; cursor: text; }
+
+.searchfield {
+text-align: right;
+position: relative;
+}
 
 </style>
